49d4e9eff7943f3ad0e36ff9bc3948d79ab4e122
Veerbhadra2006 <161051175+Veerbhadra2006@users.noreply.github.com>
2026-03-12 16:38:02 +0530

    initial commit

---
+++ area-of-square-side.py
@@ -0,0 +1,5 @@
+side = float(input("Enter the side of square : "));
+
+area = side ** 2;
+
+print("The area of th square is : ", area)
---
+++ sum-of-two-number.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter the first Number : "));
+num2 = int(input("Enter the second Number : "));
+
+sum = num1 + num2;
+
+print("The sum of two number is : ", sum)
