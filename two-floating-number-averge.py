77903e44145ae18c2c9b3103ee516df69c9c2693
Veerbhadra2006 <161051175+Veerbhadra2006@users.noreply.github.com>
2026-03-12 16:54:09 +0530

    two floating number average

---
+++ two-floating-number-averge.py
@@ -0,0 +1,7 @@
+num1 = float(input("Enter the first numbers : "))
+
+num2 = float(input("Enter the second number : "))
+
+average = (num1 + num2) / 2;
+
+print("The average of floating number is : ", average)
