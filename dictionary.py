2eccc763c53d6d2f4d47d8f709d02ae141854dea
Veerbhadra2006 <161051175+Veerbhadra2006@users.noreply.github.com>
2026-03-17 09:37:54 +0530

    dictionary sums - list of subjects, one classroom is required for 1 subjects - how many classroom are needed by all students

---
+++ dictionary.py
@@ -0,0 +1,5 @@
+# you are given a list of subjects for student, assume one classroom is required for 1 subjects - how many classroom are needed by all students
+
+subjects = {"python", "java", "c++", "python", "javascript", "java", "python", "java", "c++", "c"}
+
+print(len(subjects))
