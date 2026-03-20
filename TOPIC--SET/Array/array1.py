d30737dd0315aeee481479b5a4f05d403778ec03
Veerbhadra2006 <161051175+Veerbhadra2006@users.noreply.github.com>
2026-03-20 18:47:46 +0530

    array1 file update

--- TOPIC--SET/Array/array1.py
+++ TOPIC--SET/Array/array1.py
@@ -1,12 +1,27 @@
+# from array import *
+
+# arr = array("i", [])
+
+# n = int(input("Enter the number: "))
+
+# for i in range(n):
+#     arr.append(int(input("Enter the next input: ")))
+
+# # printing outside loop
+# for x in arr:
+#     print(x, end=" ")
+    
+    
 from array import *
 
 arr = array("i", [])
 
-n = int(input("Enter the number: "))
+n = int(input("Enter te number: "))
 
 for i in range(n):
     arr.append(int(input("Enter the next input: ")))
-
-# printing outside loop
+    
 for x in arr:
     print(x, end=" ")
+    
+    
