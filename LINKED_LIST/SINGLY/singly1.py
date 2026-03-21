699cad4fd71c72f2091290f75fc7541587a4e274
Veerbhadra2006 <161051175+Veerbhadra2006@users.noreply.github.com>
2026-03-21 11:37:59 +0530

    rewrite the code to practice the logic

--- LINKED_LIST/SINGLY/singly1.py
+++ LINKED_LIST/SINGLY/singly1.py
@@ -3,27 +3,37 @@ class Node:
         self.data = data
         self.next = None
         
-# creating nodes 
+# creating a nodes 
 node1 = Node(10);
 node2 = Node(20);
 node3 = Node(30);
 node4 = Node(40);
 node5 = Node(50);
+node6 = Node(60);
+node7 = Node(70);
+node8 = Node(80);
+node9 = Node(90);
+node10 = Node(100);
 
-# connecting nodes each other for form singly linked list
+
+
+# connecting a nodes each other to form a singly linkd list
 node1.next = node2;
 node2.next = node3;
 node3.next = node4;
 node4.next = node5;
+node5.next = node6;
+node6.next = node7;
+node7.nex = node8;
+node8.next = node9;
+node9.next = node10;
 
 
-# printing the linked list
+# printing the singly linked list
 
 current = node1;
 while current is not None:
-    print(current.data , end = " -> ");
-    current = current.next;
-
-print("None")
+    print(current.data, end=" -> ")
+    current = current.next
     
-
+print("NONE")
