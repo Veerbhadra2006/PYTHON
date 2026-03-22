e33a06170bfea2c59992ab14c35fc0b01bd3b330
Veerbhadra2006 <161051175+Veerbhadra2006@users.noreply.github.com>
2026-03-22 19:09:17 +0530

    print array element with the for loop enchanced version

--- ARRAY/FOR_LOOP/for_1_.py
+++ ARRAY/FOR_LOOP/for_1_.py
@@ -4,3 +4,14 @@ val = array.array("i", [1, 2, 3, 4, 5, 6, 7]);
 
 for i in range(0, len(val)):
     print(i, end=" ")
+    
+    
+    
+    
+    # ENCHANCED FOOR LOOP
+
+import array
+val = array.array("i", [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+
+for x in val:
+    print(x, end = " ")
