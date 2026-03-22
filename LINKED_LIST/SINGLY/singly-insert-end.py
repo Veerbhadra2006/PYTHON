60afd793cda73bb29a2b504499f547a0536b1a97
Veerbhadra2006 <161051175+Veerbhadra2006@users.noreply.github.com>
2026-03-22 14:10:09 +0530

    inserting new node at the  ending in singly linked list

---
+++ LINKED_LIST/SINGLY/singly-insert-end.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        
+# creating node
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
+node4 = Node(40)
+
+# connecting node to each other to form a linked list
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+# Adding new node with data 50 at the end 
+new_node = Node(50)
+head = node1
+current = head
+
+while current.next is not None:
+    current = current.next 
+
+current.next = new_node
+    
+   
+current  = head 
+while current is not None:
+    print(current.data , end=" -> ")
+    current = current.next
