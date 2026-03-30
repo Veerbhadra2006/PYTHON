5c3df502c5d48a2166c59c2e8f8f8d30f43e8bca
Veerbhadra2006 <161051175+Veerbhadra2006@users.noreply.github.com>
2026-03-30 11:28:37 +0530

    stack - LIFO - (operations):- push , pop , print

--- STACK/stack1.py
+++ STACK/stack1.py
@@ -1,29 +1,31 @@
 class stack:
     def __init__(self):
         self.s = []
-
+        
+    # find length
     def length(self):
         return len(self.s)
-
+    
     # insert element
-    def push(self, value):
+    def push(self,value):
         self.s.insert(0, value)
-
+      
+    # check length is empty  
     def peek(self):
         if len(self.s) == 0:
             raise Exception("Stack is Empty")
         return self.s[0]
-
+    
+    # delete the element
     def pop(self):
         if len(self.s) == 0:
             raise Exception("Stack is Empty")
         return self.s.pop(0)
-
+    
     def stackPrint(self):
         for x in self.s:
-            print(x, end=" - ")
-
-
+            print(x, end=" ")
+            
 
 obj = stack()
 
@@ -32,10 +34,11 @@ obj.push(20)
 obj.push(30)
 obj.push(40)
 obj.push(50)
+obj.push(60)
+obj.push(70)
+
 
 obj.stackPrint()
-obj.pop()
+print("\n\nPOP Element is :",obj.pop(),"\n")
 
 obj.stackPrint()
-
-
