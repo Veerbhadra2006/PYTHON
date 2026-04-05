7a3386bd9e5342d8ebb3e55f825721426079bd93
Veerbhadra2006 <161051175+Veerbhadra2006@users.noreply.github.com>
2026-04-05 18:28:41 +0530

    BinarY Tree with post order structure

---
+++ TREE/postorder.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.data = value
+        
+#inserting the node in the binary tree form of POST ORDER STRUCUTRE
+def postOrder(root):
+    if (root != None):
+        postOrder(root.left)
+        postOrder(root.right)
+        print(root.data, end=" ")
+       
+#input value in the node 
+root = Node(1)
+root.left = Node(3)
+root.right = Node(5)
+root.left.left = Node(2)
+root.left.right = Node(4)
+root.right.right = Node(8)
+        
+# call the function to disply the elements from the binary tree
+postOrder(root)
