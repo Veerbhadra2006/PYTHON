ced7846bee458805d913bb200821e100f871bfcc
Veerbhadra2006 <161051175+Veerbhadra2006@users.noreply.github.com>
2026-04-13 11:35:17 +0530

    take pre set age value and print

---
+++ PRACTICE 1/DEMO.py
@@ -0,0 +1,6 @@
+# pre set value - ab jo value hamein milenge yo int type ki hogi
+
+#hamne pahale hi set kar diya hai ki age ki vlaue int data type ki hogi agar nahi to error ayega
+
+val = int (input("enter some value : "))
+print(type(val), val)
