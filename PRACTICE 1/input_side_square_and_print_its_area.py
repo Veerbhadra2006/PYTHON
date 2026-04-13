b55196c100d0910f5974fb667731c509d7062f42
Veerbhadra2006 <161051175+Veerbhadra2006@users.noreply.github.com>
2026-04-13 12:07:22 +0530

    write a program to find the are of square

---
+++ PRACTICE 1/input_side_square_and_print_its_area.py
@@ -0,0 +1,9 @@
+# write a program to input side and calculate area of square
+
+side = float(input("enter the side of sqaure : "))
+
+area = side ** 2  # ye power operator hai
+
+print("Area of the square is : ", area)
+
+
