fe664202d83c3e6d698d84dc6b82b428eaf4460a
Veerbhadra2006 <161051175+Veerbhadra2006@users.noreply.github.com>
2026-04-13 11:38:57 +0530

    .

--- PRACTICE 1/DEMO.py
+++ PRACTICE 1/DEMO.py
@@ -2,5 +2,22 @@
 
 #hamne pahale hi set kar diya hai ki age ki vlaue int data type ki hogi agar nahi to error ayega
 
-val = int (input("enter some value : "))
-print(type(val), val)
+
+
+
+
+# val = int (input("enter some value : "))
+# print(type(val), val)
+
+
+
+
+name = input("Enter name : ")
+age = int(input("enter age : "))
+marks = float(input("Enter marks : "))
+
+print("welcome", name)
+print("age", age)
+print("marks", marks)
+
+
