6a2e73fce91608dc7c43f4486ba45017cf8d6899
Veerbhadra2006 <161051175+Veerbhadra2006@users.noreply.github.com>
2026-04-15 12:18:40 +0530

    wwite a program to reverse an array - with the help of 3 different methods

--- PRACTICE 1/reverse_array.py
+++ PRACTICE 1/reverse_array.py
@@ -1,8 +1,40 @@
-# write a program to reverse an array
+# ---------------------------   write a program to reverse an array (METHOD - 1 ) ---------------------------------
+#REVERSE ARRAY WITH THE HELP OF reverse built-in method
+
+# arr = [25, 45, 32, 65, 67, 45, 20, 12, 2, 4]
+
+# arr.reverse()
+
+# print(arr)
+    
+    
+    
+
+
+
+
+# ---------------------------   write a program to reverse an array (METHOD - 1 )  ---------------------------------
+# REVERSE ARRAY WITH THE HELP OF slice method
+
+# arr = [25, 45, 32, 65, 67, 45, 20, 12, 2, 4]
+
+# reversed_arr = arr[::-1]
+# print("The reversed array is : ", reversed_arr)
+
+
+
+
+
+
+
+# ---------------------------   write a program to reverse an array (METHOD - 3 )  ---------------------------------
+#REVERSE ARRAY WITH THE HELP OF LOOP
 
 arr = [25, 45, 32, 65, 67, 45, 20, 12, 2, 4]
 
-arr.reverse()
+reversed_arr = []
 
-print(arr)
+for i in range(len(arr)-1, -1, -1):
+    reversed_arr.append(arr[i])
     
+print("THE REVERSED ARRAY IS : ", reversed_arr)
