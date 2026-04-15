eb39ba09dcd54a7aed9518a4efe7ade152378800
Veerbhadra2006 <161051175+Veerbhadra2006@users.noreply.github.com>
2026-04-15 17:14:30 +0530

    write a program to count a even or odd number in from array

--- PRACTICE 1/DEMO.py
+++ PRACTICE 1/DEMO.py
@@ -1,11 +1,20 @@
 # Write a program to count the even / odd numbers
 
+# even = 24, 32, 12, 98, 20, 40
+
+# odd = 45, 33, 5, 31
 
 
-num1 = int(input("Enter the number : "));
+arr = [24, 45, 32, 12, 98, 33, 20, 5, 40, 31]
 
-if num1 % 2 == 0:
-    print("The entered numebr is even ")
-    
-else:
-    print("The entered numbers is odd ")
+evenCounter = 0
+oddCounter = 0
+
+for i in arr:
+    if i % 2 == 0:
+        evenCounter = evenCounter + 1
+    else:
+        oddCounter = oddCounter + 1
+        
+print("the total even number is : ", evenCounter)
+print("the total odd number is  : ", oddCounter)
