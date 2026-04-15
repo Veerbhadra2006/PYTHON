300967868493830cc7f5ede6c4efeaa0c25fea7d
Veerbhadra2006 <161051175+Veerbhadra2006@users.noreply.github.com>
2026-04-15 10:49:10 +0530

    write a program to find the heigest value in array or list

--- PRACTICE 1/DEMO.py
+++ PRACTICE 1/DEMO.py
@@ -1,3 +1,13 @@
-#write a program to find the lowest value in a list
+# write a program to find the heighest value in array or list
 
-arr =
+arr = [80, 3, 24, 56, 76, 85, 2, 4]
+
+arr.append(2)
+
+maxVal = arr[0]
+
+for i in arr:
+    if i > maxVal:
+        minVal = i
+
+print("The mximum value in array is : ", minVal)
