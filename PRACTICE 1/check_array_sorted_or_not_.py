32f9b8ee8812687d1eda3350896ce7b3dabb3e06
Veerbhadra2006 <161051175+Veerbhadra2006@users.noreply.github.com>
2026-04-16 16:25:05 +0530

    write a program to check if array is sorted or not

--- PRACTICE 1/check_array_sorted_or_not_.py
+++ PRACTICE 1/check_array_sorted_or_not_.py
@@ -1,1 +1,17 @@
 # check if array is sorted or not
+
+# arr = [80, 20, 60, 30, 40, 10, 50, 70]
+
+arr = [10, 20, 30, 40, 50, 60, 70, 80];
+
+issorted = True;
+
+for i in range(len(arr) -1):
+    if arr[i] > arr[i + 1]:
+        issorted = False
+        break
+
+if issorted:
+    print("The array is sorted");
+else:
+    print("the array is not sorted")
