f9d82765431127f9e7a65f61bbf2cb26b8b7eb50
Veerbhadra2006 <161051175+Veerbhadra2006@users.noreply.github.com>
2026-04-17 16:42:28 +0530

    none

--- PRACTICE 1/DEMO.py
+++ PRACTICE 1/DEMO.py
@@ -1,19 +1,2 @@
-# write a program  Moves zeros to end  of the array 
-
-arr = [10, 0, 20, 30, 0, 40, 50]
-
-insert_pos = 0 
-
-#move non zero elements to the front. (keeping) their order 
-for x in arr:
-    if x != 0:
-        arr[insert_pos] = x
-        insert_pos += 1
-
-
-# Fill the remaining position with zeros
-for i in range(insert_pos, len(arr)):
-    arr[i] = 0
-    
-print(arr)
+# write a program to rotate array by k
 
