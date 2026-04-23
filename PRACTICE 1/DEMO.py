ac32fa3ab753007c8b39df0572505ab9e0f6e2a6
Veerbhadra2006 <161051175+Veerbhadra2006@users.noreply.github.com>
2026-04-23 11:27:59 +0530

    write a program to count a total words present in the sentence

--- PRACTICE 1/DEMO.py
+++ PRACTICE 1/DEMO.py
@@ -1,17 +1,10 @@
-# write a program to check a string 1 rotatin of string 2
+# write a program to count a words
 
-string1 = "abcde"
+string = "Rajesh"
 
-string2 = "cdeab"
+word = 0
 
-# step1 length check 
-if len(string1) != len(string2):
-    print("No, rotation nahi hai")
-else:
-    temp = string1 + string1
-    
-#step2 check substring
-if string2 in temp:
-    print("Yes, rotation hai")
-else:
-    print("No, rotation nahi hai")
+for char in string:
+    word = word+1
+
+print("The total word in string is : ", word)
---
+++ PRACTICE 1/STRING/Count_word.py
@@ -0,0 +1,9 @@
+# write a program to count a word
+
+text = input("Enter a sentence: ")
+
+words = text.split()
+print("Number of word : ", len(words))
+
+
+ 
