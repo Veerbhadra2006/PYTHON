973481764102d596fcf137f2cbb28aa20a8dd1da
Veerbhadra2006 <161051175+Veerbhadra2006@users.noreply.github.com>
2026-04-24 15:20:11 +0530

    write a program string compression

--- PRACTICE 1/STRING/String_compression.py
+++ PRACTICE 1/STRING/String_compression.py
@@ -1,4 +1,4 @@
-# write a program String compression 
+# write a program String compression
 
 text = input("Enter a string : ")
 
@@ -7,9 +7,9 @@ count = 1
 
 for i in range(len(text)):
     if i < len(text) - 1 and text[i] == text[i + 1]:
-        count += 1
+        count = count + 1
     else:
-        result += text[i] + str(count)
+        result = result + text[i] + str(count)
         count = 1
         
-print("Compressed string : ", result)
+print("compressed String : ", result)
