a6f2989d01f64137c017ec7028804a6bdb91711e
Veerbhadra2006 <161051175+Veerbhadra2006@users.noreply.github.com>
2026-04-27 10:27:03 +0530

    write a program to find the odd and even number

---
+++ PRACTICE 2/Array/array8.py
@@ -0,0 +1,10 @@
+# wrote a program to find the odd and even number in the 
+
+num1 = int(input("Enter the number is odd or even : "))
+
+if num1 % 2 == 0:
+    print("Entered number is even : ", num1)
+    
+else:
+    print("Enven number is odd : ", num1)
+    
