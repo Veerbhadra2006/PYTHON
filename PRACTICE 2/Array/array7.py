f046317b2ffa2fd12716d37375a7352abe165bfe
Veerbhadra2006 <161051175+Veerbhadra2006@users.noreply.github.com>
2026-04-27 09:55:34 +0530

    write a program to find the missing from an array

---
+++ PRACTICE 2/Array/array7.py
@@ -0,0 +1,18 @@
+# write a program to fnd the missing numbers from an array
+
+arr = [1, 2, 4, 5]
+
+n = len(arr) + 1
+
+total_sum = n * (n + 1) // 2
+
+arr_sum = sum(arr)
+
+missing = total_sum - arr_sum
+
+arr.append(missing)
+
+arr.sort()
+
+print(arr)
+
